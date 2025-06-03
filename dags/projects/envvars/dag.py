4ef5135aa0df1cbf725d4c2194edbafdfde1af9f
Bruno Peixoto <brunolnetto@gmail.com>
2025-06-03 17:01:32 -0300

    feat: environment variables

--- dags/projects/envvars/dag.py
+++ dags/projects/envvars/dag.py
@@ -4,8 +4,8 @@ from datetime import datetime
 import os
 
 def print_env():
-    print("AWS_ACCESS_KEY_ID:", os.getenv("AWS_ACCESS_KEY_ID"))
-    print("AWS_SECRET_ACCESS_KEY:", os.getenv("AWS_SECRET_ACCESS_KEY"))
+    print("MINIO_ACCESS_KEY:", os.getenv("MINIO_ACCESS_KEY"))
+    print("MINIO_SECRET_KEY:", os.getenv("MINIO_SECRET_KEY"))
 
 with DAG("debug_env_dag", start_date=datetime(2024, 1, 1), schedule_interval=None, catchup=False) as dag:
     t1 = PythonOperator(
